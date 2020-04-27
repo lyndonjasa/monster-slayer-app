6c2e200e1d5436eb77cb4d724e2d9aab3dcf1858
Don Jasa <LyndonJ@magenic.com>
2020-04-27 10:47:18 +0800

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+// server.js
+var express = require('express');
+
+const app = express();
+const pathName = __dirname + "/site";
+app.use(express.static(pathName));
+app.get("/*", (req, res) => {
+    res.sendFile(__dirname + "/site/index.html");
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
